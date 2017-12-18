1af1ad14e6afdfdf88e659dee54263896f9a1acb
leeebucks <leeebucks@gmail.com>
2017-12-18 15:58:12 +0800

    Update final.py

--- final.py
+++ final.py
@@ -6,6 +6,7 @@ import csv
 import keras
 import logging
 import numpy as np
+import plot_acc_loss
 import tensorflow as tf
 import keras.utils.vis_utils
 import matplotlib.pyplot as plt
@@ -115,26 +116,6 @@ model.summary()
 
 # 绘制acc及loss曲线
 
-def plot_acc_loss(result, epochs):
-    acc = result.history['acc']
-    loss = result.history['loss']
-    val_acc = result.history['val_acc']
-    val_loss = result.history['val_loss']
-    plt.figure(figsize=(15, 5))
-    plt.subplot(121)
-    plt.plot(range(epochs), acc, label='Train_acc')
-    plt.plot(range(epochs), val_acc, label='Test_acc')
-    plt.title('Accuracy over ' + str(epochs) + ' Epochs', size=15)
-    plt.legend()
-    plt.grid(True)
-    plt.subplot(122)
-    plt.plot(range(epochs), loss, label='Train_loss')
-    plt.plot(range(epochs), val_loss, label='Test_loss')
-    plt.title('Loss over ' + str(epochs) + ' Epochs', size=15)
-    plt.legend()
-    plt.grid(True)
-    plt.show()
-    
 plot_acc_loss(result, epochs)
 
 # 最后验证，使用生成器
